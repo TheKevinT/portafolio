28b80bae67608887f16ff5a50563604a31e86836
Kevin Tipantiza <kevin.tipantiza@epn.edu.ec>
2020-10-08 15:27:57 -0500

    certificados realizado con bootstrap

--- src/components/certificate/certificate.js
+++ src/components/certificate/certificate.js
@@ -1,12 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+
+import Certi  from '../../assets/img_certificate/dataCerti'
 
 
 class Certificate extends Component{
 
     render(){
         return(
+            <Fragment>
+                <div className="col-md-12">
+                    <h2 className="tittle-certificate">Cursos y Certificaciones</h2>
 
-            <h1>Certificate</h1>
+                    {
+                        Certi.map((certi, i)=>{
+
+                            return (
+                                <div key={i} className="container">
+                                    <h4>{certi.title}</h4>
+                                      <img className="img-thumbnail"   src = {certi.img}  alt="certificado"/>
+                                      <hr/>
+                                </div>
+                              
+                            )
+
+                        })
+
+                    }
+
+                </div>
+        
+            </Fragment>
+
+         
         )
     }
 
